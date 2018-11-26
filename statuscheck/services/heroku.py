ef18e4594c6fdee2598465e3d69686525b172dc0
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2018-11-26 20:48:52 +0100

    Improve Heroku statuses handling

--- statuscheck/services/heroku.py
+++ statuscheck/services/heroku.py
@@ -20,10 +20,19 @@ class ServiceAPI(BaseServiceAPI):
         response.raise_for_status()
         return response.json()
 
+    def _is_status_good(self, status):
+        return self.STATUS_TYPE_MAPPING.get(status, '') == TYPE_GOOD
+
     def get_status(self):
         if not self.data:
             self.data = self._get_status_data()
-        return self.data['status']['Production']
+        status = status_prod = self.data['status']['Production']
+        status_dev = self.data['status']['Development']
+        if not self._is_status_good(status_dev):
+            status = status_dev
+        if not self._is_status_good(status_prod):
+            status = status_prod
+        return status
 
     def get_type(self):
         status = self.get_status()
