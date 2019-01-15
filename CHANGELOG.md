aadd1c90a4b213b74bee68f33a3df9d90ca8c97f
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2019-01-15 20:56:13 +0100

    Update GitHub service code, add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+### Added
+
+### Changed
+
+### Removed
+
+## [1.0.0] - 2019-01-15
+### Added
+- Changelog file
+
+### Changed
+- Update GitHub service code, since they moved to statuspage.io
+- Update dependencies
+
+[Unreleased]: https://github.com/amureki/statuscheck/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/amureki/statuscheck/compare/v.0.1.0...v1.0.0
--- statuscheck/services/github.py
+++ statuscheck/services/github.py
@@ -1,39 +1,6 @@
-import requests
-
-from statuscheck.services._base import BaseServiceAPI
-from statuscheck.status_types import TYPE_OUTAGE, TYPE_INCIDENT, TYPE_GOOD
+from statuscheck.services._statuspage import BaseStatusPageAPI
 
 
-class ServiceAPI(BaseServiceAPI):
-    STATUS_TYPE_MAPPING = {
-        'good': TYPE_GOOD,
-        'minor': TYPE_INCIDENT,
-        'major': TYPE_OUTAGE,
-    }
-
-    base_url = 'https://status.github.com/api/'
-    status_url = 'https://status.github.com'
-
-    def _get_status_data(self):
-        url = self.base_url + 'last-message.json'
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-
-    def get_status(self):
-        if not self.data:
-            self.data = self._get_status_data()
-        return self.data['status']
-
-    def get_type(self):
-        status = self.get_status()
-        status_type = self.STATUS_TYPE_MAPPING.get(status, '')
-        if not status_type:
-            self.capture_log(status)
-        return status_type
-
-    def get_active_incident(self):
-        status_type = self.get_type()
-        if status_type == TYPE_GOOD:
-            return ''
-        return self.data.get('body') or self.get_status()
+class ServiceAPI(BaseStatusPageAPI):
+    domain_key = 'kctbh9vrtdwd'
+    status_url = 'https://www.githubstatus.com/'
