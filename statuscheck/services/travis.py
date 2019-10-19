1fca67ad59e724635c0a7e2403126671a4be387b
Rustem Saiargaliev <rustem.sayargaliev@thermondo.de>
2019-10-19 11:31:34 +0200

    Add trailing slashes, fix email

--- statuscheck/services/travis.py
+++ statuscheck/services/travis.py
@@ -3,4 +3,4 @@ from statuscheck.services._statuspage import BaseStatusPageAPI
 
 class ServiceAPI(BaseStatusPageAPI):
     domain_key = 'pnpcptp8xh9k'
-    status_url = 'https://www.traviscistatus.com'
+    status_url = 'https://www.traviscistatus.com/'
